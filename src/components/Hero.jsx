19e4c078c73d8db5776727b9a7bcbda6c9c7fd9f
jitender1222 <jeetusinghkanyal@gmail.com>
2025-06-25 14:10:26 +0530

    implemented the first part of hero section

---
+++ src/components/Hero.jsx
@@ -0,0 +1,81 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { SplitText } from "gsap/all";
+import React from "react";
+
+const Hero = () => {
+  useGSAP(() => {
+    const textSplit = new SplitText(".title", { type: "words,chars" });
+    const paragraphSplit = new SplitText(".subtitle", { type: "lines" });
+
+    textSplit.chars.forEach((char) => char.classList.add("text-gradient"));
+
+    gsap.from(textSplit.chars, {
+      yPercent: 100,
+      duration: 1.8,
+      ease: "expo.out",
+      stagger: 0.1,
+    });
+
+    gsap.from(paragraphSplit.lines, {
+      opacity: 0,
+      yPercent: 100,
+      duration: 1.8,
+      ease: "expo.out",
+      delay: 1,
+      stagger: 0.1,
+    });
+
+    gsap
+      .timeline({
+        scrollTrigger: {
+          trigger: "#hero",
+          start: "top top",
+          end: "bottom top",
+          scrub: true,
+        },
+      })
+      .to(".right-leaf", { y: 400 }, 0)
+      .to(".left-leaf", { y: -400 }, 0);
+  }, []);
+  return (
+    <section id="hero" className="noisy">
+      <h1 id="title" className="title">
+        MOJITO
+      </h1>
+
+      <img
+        src="/images/hero-left-leaf.png"
+        alt="right-leaf"
+        className="left-leaf"
+      />
+
+      <img
+        src="/images/hero-right-leaf.png"
+        alt="right-leaf"
+        className="right-leaf"
+      />
+
+      <div className="body">
+        <div className="content">
+          <div className="space-y-5 hidden md:block md:mt-20">
+            <p>Cool. Crisp. Classic.</p>
+            <p className="subtitle">
+              Sip the spirit <br /> of summer
+            </p>
+          </div>
+          <div className="view-cocktails">
+            <p className="subtitle">
+              Every cocktail on our menu is a blend of premium ingredients,
+              creative flair, and timeless recipes — designed to delight your
+              senses.{" "}
+            </p>
+            <a href="#cocktail">View Cocktails</a>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import gsap from "gsap";
 import { navlinks } from "../../constants";
 import { useGSAP } from "@gsap/react";
 
