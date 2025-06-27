51f211719d30a6c683b0103339fd22bcbd58d674
jitender1222 <jeetusinghkanyal@gmail.com>
2025-06-28 00:35:57 +0530

    implement menu page

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Hero from "./components/Hero";
 import Cocktail from "./components/Cocktail";
 import About from "./components/About";
 import Art from "./components/Art";
+import Menu from "./components/Menu";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
@@ -16,6 +17,7 @@ const App = () => {
       <Cocktail />
       <About />
       <Art />
+      <Menu />
     </main>
   );
 };
